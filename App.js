bc7732bb256bbfdf117255263aa9df0acf4cd467
ezehmark <ezehmark@gmail.com>
2024-11-06 10:38:24 +0100

    check profiles

--- App.js
+++ App.js
@@ -4,46 +4,19 @@ import Menu from './menu';
 import Fund from './fund.tsx';
 import Recents from './recents.tsx';
 import Messages from './messages.tsx';
+import Profile from './profiles.tsx';
 
-const Stack = createStackNavigator();
+export default function App() {                                                                                         
 
-export default function App() {                                                                                           const [menuOpen, setMenuOpen] = useState(false);
+   return (
+	   <>
+{menuOpen && <Menu isOpen = {menuOpen} toggleMenu={toggleMenu} />}
 
-    const toggleMenu = () => {
-        setMenuOpen(prev => !prev);
-    };
-        const[openMsg, setOpenMsg]=useState(false);
-        const toggleMsg =()=>{
-                setOpenMsg(msg=>!msg);
-        }
-
-    return (
-        <>
-           <NavigationContainer initialRouteName='recents'>
-               
-	    <Stack.Navigator initialRouteName='home'>
-                   
-	    <Stack.Screen name="home" component={Home}
-            options={{headerShown:false}}initialParams={{toggleMenu, toggleMsg}}/>
-           
-	    <Stack.Screen name='recents' component={Recents} initialParams={{toggleMenu, toggleMsg}} options={{headerShown:false}}/>
-
-
-                   
-	    <Stack.Screen name="fund" component={Fund}
-            options={{headerShown:false}}initialParams={{toggleMenu, toggleMsg}}/>
-
-
-                </Stack.Navigator>
-            </NavigationContainer>
-
-
-            {menuOpen && <Menu isOpen = {menuOpen} toggleMenu={toggleMenu} />}
-
-           {openMsg && <Messages isMsg={openMsg} toggleMsg={toggleMsg}/>}
 
 	    
-        </>
+        <Profile/>
+	</>
     );
 }
+
 
