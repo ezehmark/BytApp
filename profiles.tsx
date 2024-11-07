79cc7cb6a7c027fc2e8bd40ed9dbe13c09ec5b16
ezehmark <ezehmark@gmail.com>
2024-11-07 06:51:21 +0100

    c profiles

--- profiles.tsx
+++ profiles.tsx
@@ -11,7 +11,9 @@ const Profile: React.FC = () => {
 
   return (
     <View style={styles.container}>
-    <TouchableOpacity onPress={toggleMenu} style={styles.menuCircle}>                                                     <Image style={styles.menuIcon} source={{ uri: 'https://i.postimg.cc/ZnGwS6pJ/Picsart-24-11-01-05-41-03-753.png' }} /> </TouchableOpacity>
+    <TouchableOpacity 
+    onPress={toggleMenu} style={styles.menuCircle}>                                                     
+    <Image style={styles.menuIcon} source={{ uri: 'https://i.postimg.cc/ZnGwS6pJ/Picsart-24-11-01-05-41-03-753.png' }} /> </TouchableOpacity>
 
     <TouchableOpacity onPress={toggleMsg} style={styles.infoCircle}>
     <Image style={styles.bellIcon} source={{ uri: 'https://i.postimg.cc/Kvhbr28G/Picsart-24-11-01-00-29-29-864.png' }} />                   </TouchableOpacity>
@@ -19,7 +21,7 @@ const Profile: React.FC = () => {
         colors={['white', '#f5b857']}
         start={{ x: 0, y: 0 }}
         end={{ x: 0, y: 1 }}
-        style={styles.gradientContainer} >
+        style={styles.gradientContainer}>
         <View style={styles.userInfoArea}>
 	<View style={styles.picContainer}>
 
@@ -30,17 +32,20 @@ const Profile: React.FC = () => {
 
           <View style={styles.userInfo}>
             <View style={styles.infoWritten1}>
-              <Text style={styles.userName}>Username:</Text>
-              <Text style={styles.userNameValue}>Ezeh Mark</Text>
+              <Text style={styles.userName}>
+	      Username:</Text>
+              <Text 
+	      style={styles.userNameValue}>Ezeh Mark</Text>
             </View>
             <View style={styles.infoWritten2}>
               <Text style={styles.rank}>Ranking:</Text>
-              <Text style={styles.rankValue}>Level 1 ⭐</Text>
+              <Text style={styles.rankValue}>
+	      Level 1 ⭐</Text>
             </View>
 
 	    <View style={styles.infoWritten3}>                                <Text style={styles.userName}>Referral ID:</Text>
-              <Text style={styles.userNameValue}>#A1014GG</Text
->                                                                           </View>
+              <Text style={styles.userNameValue}>
+	      #A1014GG</Text>                                                                           </View>
           </View>
         </View>
 
@@ -50,7 +55,7 @@ const Profile: React.FC = () => {
     end={{x:0,y:1}}
     style={styles.itemsZone}>                
         <Text style={styles.item1}>User Verification</Text>
-        <Text style={styles.item1}>Engage Customer Support 👨‍💻</Text>
+        <Text style={styles.item1}>Engage Customer Support </Text>
         <Text style={styles.item3}>Follow Us On ↓</Text>
 
 
@@ -80,22 +85,20 @@ const Profile: React.FC = () => {
             <Text style={styles.tabText}>Home</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => navigation.navigate('fund')} style={styles.tabArea}>
+          <TouchableOpacity 
+	  onPress={() => navigation.navigate('fund')} style={styles.tabArea}>
             <View style={styles.tab}>
-              <Image
-                style={styles.fundImage}
-                source={{ uri: 'https://i.postimg.cc/3RD6dnVS/Picsart-24-11-01-02-14-35-571.png' }}
-              />
+              <Image style={styles.fundImage}
+                source={{ uri: 'https://i.postimg.cc/3RD6dnVS/Picsart-24-11-01-02-14-35-571.png' }}/>
             </View>
             <Text style={styles.tabText}>Fund</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => navigation.navigate('recents')} style={styles.tabArea}>
+          <TouchableOpacity 
+	  onPress={() => navigation.navigate('recents')} style={styles.tabArea}>
             <View style={styles.tab}>
-              <Image
-                style={styles.tabImage}
-                source={{ uri: 'https://i.postimg.cc/RZHzKTXL/Picsart-24-11-01-05-09-49-049.png' }}
-              />
+              <Image style={styles.tabImage}
+                source={{ uri: 'https://i.postimg.cc/RZHzKTXL/Picsart-24-11-01-05-09-49-049.png' }}/>
             </View>
             <Text style={styles.tabText}>Recents</Text>
           </TouchableOpacity>
@@ -104,8 +107,7 @@ const Profile: React.FC = () => {
             <View style={styles.tab}>
               <Image
                 style={styles.tabImage}
-                source={{ uri: 'https://i.postimg.cc/rs3PwBXX/Picsart-24-11-01-05-26-01-447.png' }}
-              />
+                source={{ uri: 'https://i.postimg.cc/rs3PwBXX/Picsart-24-11-01-05-26-01-447.png' }}/>
             </View>
             <Text style={styles.tabText}>Profile</Text>
           </TouchableOpacity>
@@ -116,10 +118,26 @@ const Profile: React.FC = () => {
 };
 
 const styles = StyleSheet.create({
-
-	menuCircle: {                                                                                                                                                                            top: 6,                                                                                                               left: 10,                                                                                                             height: 30,                                                                                                           width: 35,                                                                                                            overflow: 'hidden',                                                                                                   zIndex:3,                                                         },                                                                                                             menuIcon: {                                                                                                                   height: 24,                                                                                                           width: 24,                                                                                                                                                                       left:8,                                    resizeMode:'contain',                                                                                                                   },
-  container: { flex: 1 },
-  gradientContainer: { flex: 1 },
+  menuCircle: {
+    top: 6,
+    left: 10,
+    height: 30,
+    width: 35,
+    overflow: 'hidden',
+    zIndex: 3,
+  },
+  menuIcon: {
+    height: 24,
+    width: 24,
+    left: 8,
+    resizeMode: 'contain',
+  },
+  container: {
+    flex: 1,
+  },
+  gradientContainer: {
+    flex: 1,
+  },
   userInfoArea: {
     position: 'absolute',
     height: 105,
@@ -131,73 +149,78 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     flexDirection: 'row',
     zIndex: 1,
-    padding:15,
-    alignItems:'center',
-    borderWidth:2,
+    padding: 15,
+    alignItems: 'center',
+    borderWidth: 2,
     borderColor: '#dd4858',
-
-    
   },
-
-infoCircle: {                                                                                                                 position: 'absolute',                                                                                                 top: 6,                                                                                                               right: 10,                                                                                                            height: 30,                                                                                                           width: 30,                                                                                                            borderRadius: 15,                                                                                                     justifyContent: 'center',                                                                                             alignItems: 'center',                                                                                                 backgroundColor: 'black',                                                                                             overflow: 'hidden',                                                                                                   zIndex: 3,                                                        },                                                                                                     bellIcon: {height: 25,                                                                                                           width: 25,                                                                                                            left:1                                                                                                        },
-  picContainer:{
-	  height:95,
-	  width:94,
-	  borderTopRightRadius:25,                                    borderBottomRightRadius:25,
-	  position: 'absolute',
-	  backgroundColor:'#dd4858',
-	  alignItems:'center',
-	  justifyContent:'center',
-	  right:2,
-
+  infoCircle: {
+    position: 'absolute',
+    top: 6,
+    right: 10,
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    overflow: 'hidden',
+    zIndex: 3,
   },
-
-  userPicCircle:{
-	  height:76,
-	  position:'absolute',
-	  width:76,
-	  borderRadius:38,
-	  backgroundColor:'blue',
-	  
-	  overflow:'hidden',
-	  borderWidth:2,
-	  borderColor:'#8cd5cd',
-
-
-	  
-
+  bellIcon: {
+    height: 25,
+    width: 25,
+    left: 1,
   },
-
-  userPic:{
-	  resizeMode:'contain',
-	  height:150,
-	  width:100,
-	  bottom:30,
+  picContainer: {
+    height: 95,
+    width: 94,
+    borderTopRightRadius: 25,
+    borderBottomRightRadius: 25,
+    position: 'absolute',
+    backgroundColor: '#dd4858',
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 2,
+  },
+  userPicCircle: {
+    height: 76,
+    position: 'absolute',
+    width: 76,
+    borderRadius: 38,
+    backgroundColor: 'blue',
+    overflow: 'hidden',
+    borderWidth: 2,
+    borderColor: '#8cd5cd',
+  },
+  userPic: {
+    resizeMode: 'contain',
+    height: 150,
+    width: 100,
+    bottom: 30,
   },
   userInfo: {
-	  position:'absolute',
+    position: 'absolute',
     height: 95,
     width: '65%',
-    left:2,
-    
+    left: 2,
     justifyContent: 'space-around',
     flexDirection: 'column',
-    alignItems:'center',
-    overflow:'hidden',
-    backgroundColor:'#dd4858',
-    borderTopLeftRadius:25,
-    borderBottomLeftRadius:25,
+    alignItems: 'center',
+    overflow: 'hidden',
+    backgroundColor: '#dd4858',
+    borderTopLeftRadius: 25,
+    borderBottomLeftRadius: 25,
   },
   infoWritten1: {
     height: 25,
     width: 150,
     justifyContent: 'space-around',
     flexDirection: 'row',
-    
-    alignItems:'center',
-    borderRadius:10,
-    borderColor:'#adafa4',
-    borderWidth:1,
+    alignItems: 'center',
+    borderRadius: 10,
+    borderColor: '#adafa4',
+    borderWidth: 1,
   },
   userName: {
     color: '#ccc',
@@ -207,32 +230,26 @@ infoCircle: {                                                                   
     color: '#8cd5cd',
     fontWeight: 'bold',
   },
-
-
   infoWritten2: {
-    
     height: 25,
     width: 150,
     justifyContent: 'space-around',
     flexDirection: 'row',
-    
-    alignItems:'center',
-    borderRadius:10,                                           borderColor:'#adafa4',                                        borderWidth:1,
+    alignItems: 'center',
+    borderRadius: 10,
+    borderColor: '#adafa4',
+    borderWidth: 1,
   },
-
-  infoWritten3: {                                                   
-    height: 25,                                                    
-    width: 150,                                                    
+  infoWritten3: {
+    height: 25,
+    width: 150,
     justifyContent: 'space-around',
     flexDirection: 'row',
-    borderColor:'#adafa4',
-    borderRadius:10,
-    borderWidth:1,
-    alignItems:"center",
-
+    alignItems: 'center',
+    borderColor: '#adafa4',
+    borderRadius: 10,
+    borderWidth: 1,
   },
-
-
   rank: {
     color: '#ccc',
     fontWeight: 'bold',
@@ -241,98 +258,85 @@ infoCircle: {                                                                   
     color: '#8cd5cd',
     fontWeight: 'bold',
   },
-
-moveable: {
-        height: 400,
-        width: '96%',
-        position: 'absolute',
-        top: 400,
-        left: '2%',
-        backgroundColor: 'blue',
-    },
-    itemsZone: {
-        position:'absolute',
-	height: 400,
-        width: '90%',
-        justifyContent: 'space-around',
-        flexDirection: 'column',
-        top: 180,
-        paddingBottom: 100,
-        backgroundColor: '#fff',
-	borderRadius:15,
-	left:'5%',
-	padding:20,
-	alignItems:'center',
-    },
-    item1: {
-        height: 50,
-        width: '80%',
-        borderColor: '#ccc',
-        borderWidth: 1,
-	alignItems:'center',
-        marginBottom: 15,
-        backgroundColor: '#8cd5cd',
-	borderRadius:10,
-	padding:15,
-	textAlign:'center',
-	justifyContent:'center',
-	
-	fontSize:15,
-	fontWeight:'bold',
-	
-    },
-
-    item3: {
-        height: 50,
-        width: '80%',
-        borderColor: '#ccc',
-        alignItems:'center',                                       marginBottom: 15,                                          backgroundColor: 'white',                                  borderRadius:10,
-        padding:15,
-        textAlign:'center',                                        justifyContent:'center',
-
-        fontSize:18,
-	fontWeight:'bold',
-	color:'red',
-
-    },
-
-    socialIcons:{
-	    position:'absolute',
-	    justifyContent:'space-around',
-
-	    flexDirection:'row',
-	    bottom:70,
-	    height:60,
-	    width:'80%',
-	    alignSelf:'center',
-	    left:'10%',
-
-
-    },
-
-    icon1:{
-	    height:50,
-	    width:50,
-	    borderRadius:25,
-	    shadowColor:"black",
-	    shadowOpacity:0.4,
-	    shadowOffset:{height:2, width:1},
-	    elevation:4,
-	    overflow:'hidden',
-	    alignItems:'center',
-            justifyContent:'center',
-
-
-
-    },
-
-    iconImg:{
-	    resizeMode:'cover',
-	    height:48,
-	    width:48,
-
-    },
-  
+  moveable: {
+    height: 400,
+    width: '96%',
+    position: 'absolute',
+    top: 400,
+    left: '2%',
+    backgroundColor: 'blue',
+  },
+  itemsZone: {
+    position: 'absolute',
+    height: 400,
+    width: '90%',
+    justifyContent: 'space-around',
+    flexDirection: 'column',
+    top: 180,
+    paddingBottom: 100,
+    backgroundColor: '#fff',
+    borderRadius: 15,
+    left: '5%',
+    padding: 20,
+    alignItems: 'center',
+  },
+  item1: {
+    height: 50,
+    width: '80%',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    alignItems: 'center',
+    marginBottom: 15,
+    backgroundColor: '#8cd5cd',
+    borderRadius: 10,
+    padding: 15,
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  item3: {
+    height: 50,
+    width: '80%',
+    borderColor: '#ccc',
+    alignItems: 'center',
+    marginBottom: 15,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 15,
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'red',
+  },
+  socialIcons: {
+    position: 'absolute',
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    bottom: 70,
+    height: 60,
+    width: '80%',
+    alignSelf: 'center',
+    left: '10%',
+  },
+  icon1: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    shadowColor: 'black',
+    shadowOpacity: 0.4,
+    shadowOffset: { height: 2, width: 1 },
+    elevation: 4,
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  iconImg: {
+    resizeMode: 'cover',
+    height: 48,
+    width: 48,
+  },
   bottomTab: {
     position: 'absolute',
     bottom: 0,
@@ -385,6 +389,12 @@ moveable: {
     textAlign: 'center',
   },
 });
+                                                                                                   
+                                                                                                    
+  
+  
+    
 
 export default Profile;
+
 
