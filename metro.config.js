bd3b18fc700a845bee8f3bf1ddf9d9ac3ecf11fe
ezehmark <ezehmark@gmail.com>
2025-06-17 23:26:33 +0100

    new build

---
+++ metro.config.js
@@ -0,0 +1,33 @@
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+    transformer,
+  } = await getDefaultConfig();
+
+  return {
+    transformer: {
+      ...transformer,
+      minifierConfig: {
+        keep_classnames: false,
+        keep_fnames: false,
+        mangle: {
+          toplevel: true,
+        },
+        output: {
+          comments: false,
+        },
+        compress: {
+          drop_console: true,
+          passes: 2,
+          toplevel: true,
+        },
+      },
+    },
+    resolver: {
+      assetExts,
+      sourceExts,
+    },
+  };
+})();
